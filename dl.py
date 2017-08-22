512300c3b7fc65d17aeca11b37ef4b9fc7afdce0
xPMo <PMo_@vivaldi.net>
2017-08-22 16:17:09 -0500

    Add Threading support and move last.txt write
    last.txt should be written before download, to avoid missing a published video while downloading others

--- dl.py
+++ dl.py
@@ -1,11 +1,21 @@
+#! /usr/bin/env python3
 import opml
 import feedparser
 import youtube_dl
 from glob import glob
 from pprint import pprint
+from threading import Thread
 
 from time import time, mktime, strptime
 from datetime import datetime
+
+def parse_one(url, videos, ptime):
+    feed = feedparser.parse(url)
+    for j in range(0,len(feed['items'])):
+        timef = feed['items'][j]['published_parsed']
+        dt = datetime.fromtimestamp(mktime(timef))
+        if dt > ptime:
+            videos.append(feed['items'][j]['link'])
 
 if len(glob('last.txt')) == 0:
     f = open('last.txt', 'w')
@@ -28,15 +38,23 @@ else:
     for i in range(0,len(outline[0])):
         urls.append(outline[0][i].xmlUrl)
 
+    threads = []
     videos = []
-    for i in range(0,len(urls)):
-        print('Parsing through channel '+str(i+1)+' out of '+str(len(urls)), end='\r')
-        feed = feedparser.parse(urls[i])
-        for j in range(0,len(feed['items'])):
-            timef = feed['items'][j]['published_parsed']
-            dt = datetime.fromtimestamp(mktime(timef))
-            if dt > ptime:
-                videos.append(feed['items'][j]['link'])
+    for url in urls:
+        new_thread = Thread(target = parse_one, args = (url, videos, ptime))
+        new_thread.start()
+        threads.append(new_thread)
+    j = 1
+    for thread in threads:
+        thread.join()
+        print('Parsed through channel '+str(j)+' out of '+str(len(urls)), end='\r')
+        j += 1
+
+    # feed just checked, so we can write out a new "last checked"
+    # (Another video may be published while we download)
+    f = open('last.txt', 'w')
+    f.write(str(ftime))
+    f.close()
 
     if len(videos) == 0:
         print('Sorry, no new video found')
@@ -48,6 +66,3 @@ else:
     with youtube_dl.YoutubeDL(ydl_opts) as ydl:
         ydl.download(videos)
 
-    f = open('last.txt', 'w')
-    f.write(str(ftime))
-    f.close()
